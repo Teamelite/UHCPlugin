f63cc4661735d77fa19f78a1bd2a4c6acc8e1e6a
dave bentley <david.bentley94@btinternet.com>
2014-12-22 21:46:11 +0000

    onEnable

--- src/uhcplugin/UHCPlugin.java
+++ src/uhcplugin/UHCPlugin.java
@@ -1,13 +1,12 @@
-
 package uhcplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
 public class UHCPlugin extends JavaPlugin {
-    
-    
 
-   
-
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("Plugin Enabled");
+    }
 }
