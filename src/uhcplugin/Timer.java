6052825f3160a77352c70417054d76d5b0d8153c
dave bentley <david.bentley94@btinternet.com>
2015-01-09 04:56:18 +0000

    fixes

---
+++ src/uhcplugin/Timer.java
@@ -0,0 +1,17 @@
+
+package uhcplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Timer implements CommandExecutor {
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        
+        return false;
+        
+    }
+
+}
